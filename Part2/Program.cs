99d80470ae73673224c23a174647f55e10edac69
Asido <asido@asidos-mbp.wireless.rit.edu>
2019-08-28 10:15:01 -0400

    My first commit on GitHub

---
+++ Part2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I'm from New York City");
+        }
+    }
+}
---
+++ Part3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello MAD students- I'm Asido Yellow-Duke");
+            
+            Console.WriteLine("I'm from New York City");
+
+        }
+    }
+}
